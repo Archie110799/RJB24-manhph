b34dd8bbda44cdd57b33fac541e671af6e9b445f
manhph <manhph@vnext.vn>
2022-08-31 20:19:36 +0700

    add code js with reactjs

--- 2-examples/src/App.js
+++ 2-examples/src/App.js
@@ -1,12 +1,68 @@
+import React from "react";
+import { act } from "react-dom/test-utils";
 import "./App.css";
 import JSX from "./Examples/JSX";
 import image1 from "./images/1.jpeg";
 
+let a = 12
+
+let c = {
+  id: 1,
+  name: "User 1",
+};
+
+let d = {
+  id: 2,
+  name: "User 2",
+};
+
+
+const arrayNew = [
+  {id : 3, name: 'ba' },
+  {id :4, name : 'bon'},
+  {id :5, name : 'nam'}
+]
+
+const _render = (input) => {
+  return (
+    <>
+      <p>{input.id}</p>
+      <p style={{ color: input.id % 2 === 0 ? "red" : "green" }}>
+        {input.name}
+      </p>
+    </>
+  );
+};
+
+for (let index = 0; index < arrayNew.length; index++) {
+  let item = arrayNew[index]
+  _render(item)
+}
+
+arrayNew.map(
+  (item) => {return _render(item)} 
+)
+
+arrayNew.map(
+  (item) => _render(item)
+)
 
 function App() {
   return (
     <>
-      <JSX />
+      {_render(c)}
+      {_render(d)}
+
+      {_render(arrayNew[0])}
+      {_render(arrayNew[1])}
+      {_render(arrayNew[2])}
+
+      {
+        arrayNew.map(
+          (item) => <div key={item.id}>{_render(item)}</div>
+        )
+      }
+      
     </>
   );
 }
